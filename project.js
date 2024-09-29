e7804026ffb45f20296695be5a706c9cd4a55905
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2024-09-29 02:03:40 +0200

    zavrti & proveriDobitak finished

--- project.js
+++ project.js
@@ -1,5 +1,11 @@
 const ulaz = require("prompt-sync")();
 
+const matricaAparata = [
+  ["a", "a", "a"],
+  ["b", "b", "b"],
+  ["c", "c", "c"],
+];
+
 function getPocetniDepozit() {
   while (true) {
     const depositAmount = parseFloat(ulaz("Unesite iznos pocetnog depozita: "));
@@ -16,33 +22,41 @@ function getRandomInt(max) {
 }
 
 function zavrti() {
-  for (a = 0; a < 250; a++) {
-    for (i = 0; i < 3; i++) {
-      // console.log(simbol)
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
       broj = Math.random();
-      if (broj < 0.40) {
-        matricaAparata[0][i] = "D";
-      } else if (0.40 < broj < 0.70) {
-        matricaAparata[0][i] = "C";
-      } else if (0.70 < broj < 0.90) {
-        matricaAparata[0][i] = "B";
+      // console.log(broj);
+      if (broj < 0.4) {
+        matricaAparata[i][j] = "D";
+      } else if (broj >= 0.4 && broj < 0.7) {
+        matricaAparata[i][j] = "C";
+      } else if (broj >= 0.7 && broj < 0.9) {
+        matricaAparata[i][j] = "B";
       } else {
-        matricaAparata[0][i] = "A";
+        matricaAparata[i][j] = "A";
       }
     }
-    console.log(matricaAparata[0]);
-    console.log(matricaAparata[1]);
-    console.log(matricaAparata[2]);
   }
+
+  console.log(matricaAparata[0]);
+  console.log(matricaAparata[1]);
+  console.log(matricaAparata[2]);
 }
 
-let matricaAparata = [
-  ["a", "a", "a"],
-  ["b", "b", "b"],
-  ["c", "c", "c"],
-];
+function proveriDobitak() {
+  for (let i = 0; i < 3; i++) {
+    if (
+      matricaAparata[i][0] === matricaAparata[i][1] &&
+      matricaAparata[i][1] === matricaAparata[i][2]
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
 
 zavrti();
+console.log(proveriDobitak());
 
 // let kredit = getPocetniDepozit();
 
